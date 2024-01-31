c832470a9ba43b44446ebed43d61d3ce86bfebaa
Luis Martinez <mrluismartinezzz@gmail.com>
2024-01-30 20:43:17 -0800

    reorganized public structure

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/static")));
 
 app.use(
   session({
